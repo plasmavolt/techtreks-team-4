4d6e33634ceaeb6e65c6244038487b1d677eb6fa
PlasmaVolt <61515425+PlasmaVolt@users.noreply.github.com>
2025-12-03 20:05:00 -0500

    quick bugfix

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -17,7 +17,7 @@ export default function TabLayout() {
         tabBarButton: HapticTab,
       }}>
       <Tabs.Screen
-        name="map"
+        name="index"
         options={{
           title: 'Map',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="map.fill" color={color} />,
